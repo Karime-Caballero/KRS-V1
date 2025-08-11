27cb258ad31fea373fc6ea73a644d8689e491094
Salvadorvga2004 <116208397+Salvadorvga2004@users.noreply.github.com>
2025-08-10 19:34:48 -0600

    Se cambio la conexion a mongo Atlas

--- Backend/src/keys.ts
+++ Backend/src/keys.ts
@@ -1,6 +1,6 @@
 export default {
     mongo: {
-        uri: 'mongodb://localhost:27017',
-        dbName: 'recetas'
-    }
+        uri: 'mongodb+srv://rl4357011:cjIZTFJntLwmqOLR@cluster0.sike1gh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+        dbName: 'Cluster0'
+    } 
 };
